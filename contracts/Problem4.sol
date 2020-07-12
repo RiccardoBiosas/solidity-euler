ec1f167ecd827d95f4f942e069af604ad0026b32
RiccardoBiosas <rbios@protonmail.com>
2020-07-12 20:41:37 +0200

    :construction: Solution to 4th project euler problem.

---
+++ contracts/Problem4.sol
@@ -0,0 +1,76 @@
+pragma solidity ^0.4.22;
+// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+// Find the largest palindrome made from the product of two 3-digit numbers.
+contract Problem4 {
+    function uintToString(uint v) internal pure returns (string memory str) {
+        uint maxlength = 100;
+        bytes memory reversed = new bytes(maxlength);
+        uint i = 0;
+        while (v != 0) {
+            uint remainder = v % 10;
+            v = v / 10;
+            reversed[i++] = byte(48 + remainder);
+        }
+        bytes memory s = new bytes(i + 1);
+        for (uint j = 0; j <= i; j++) {
+            s[j] = reversed[i - j];
+        }
+        str = string(s);
+    }
+    function stringToUint(string memory s) internal pure returns (uint) {
+        bytes memory b = bytes(s);
+        uint result = 0;
+        for (uint i = 0; i < b.length; i++) { 
+            if (b[i] >= 48 && b[i] <= 57) {
+                result = result * 10 + (uint(b[i]) - 48);
+            }
+        }
+        return result;
+    }
+    function reverseNum(uint256 _base) internal pure returns(uint256) {
+        string memory numToStr = uintToString(_base);
+        bytes memory _baseBytes = bytes(numToStr);
+        assert(_baseBytes.length > 0);
+        string memory _tempValue = new string(_baseBytes.length);
+        bytes memory _newValue = bytes(_tempValue);
+        for(uint i=0; i<_baseBytes.length; i++) {
+            _newValue[_baseBytes.length - i - 1] = _baseBytes[i];
+        }
+        string memory strResult = string(_newValue);
+        return stringToUint(strResult);
+    }
+    function checkPalindromeNum(uint256 _a) internal pure returns(bool) {
+        if(_a == reverseNum(_a)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    function productOfThreeDigits() public pure returns(uint256) {
+        uint256 firstN = 999;
+        // uint256 secondN = 999;
+        bool successor = false;
+        uint256 currentPalindrome = 0;
+        for(uint256 i = firstN; i>=100; i--) {
+            for(uint256 k = firstN; k >= 100; k--) {
+                uint256 product = k * i;
+                if(product < currentPalindrome) {
+                    break;
+                }                
+                bool isPalindrome = checkPalindromeNum(product);
+                if(isPalindrome) {
+                    if(product > currentPalindrome) {
+                        if(successor == false) {
+                            successor = true;
+                            currentPalindrome = product;
+                    } else {
+                        currentPalindrome = product;  
+                        break;                                                 
+                        }
+                    }
+                }
+            }
+        }
+        return currentPalindrome;
+    }
+}
--- truffle-config.js
+++ truffle-config.js
@@ -4,16 +4,6 @@ require('dotenv').config()
 
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
@@ -33,18 +23,13 @@ module.exports = {
         return new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA_KOVAN);
       },
       network_id: 42
-    },
-
-
-   
+    },   
   },
 
-  // Set default mocha options here, use special reporters etc.
   mocha: {
     timeout: 1000000
   },
 
-  // Configure your compilers
   compilers: {
     solc: {
       version: "0.4.22",    // Fetch exact version from solc-bin (default: truffle's version)
